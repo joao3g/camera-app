ced922001761e3c5f64675fac8405701146e45c7
Joao Gabriel <joao.almeida.gabriel2015@gmail.com>
2023-05-23 11:18:20 -0300

    Trabalho concluido

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePhoto = async () => {
+    if (camera) {
+      const photoData = await camera.takePictureAsync(null);
+      setPhoto(photoData.uri);
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        ref={(ref) => setCamera(ref)}
+        style={styles.camera}
+        type={Camera.Constants.Type.back}
+        autoFocus={Camera.Constants.AutoFocus.on}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={takePhoto}>
+        <Text style={styles.buttonText}>Capture</Text>
+      </TouchableOpacity>
+
+      {photo && <Image source={{ uri: photo }} style={styles.preview} />}
     </View>
   );
 }
@@ -16,5 +52,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    width: '100%',
+    height: '70%',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+  preview: {
+    width: 300,
+    height: 300,
+    marginTop: 20,
   },
 });
